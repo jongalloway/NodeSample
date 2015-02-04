0d56bcce2ff26a51e13f8455a64fb11b5a28331c
matvelloso <Ag-04650089>
2015-02-03 17:27:10 -0800

    parsing metadata

--- public/javascripts/corsCallSvc.js
+++ public/javascripts/corsCallSvc.js
@@ -1,24 +1,30 @@
 ﻿'use strict';
 angular.module('corsApp')
 .factory('corsCallSvc', ['$http', function ($http) {
-    return {
-        getItems: function () {
-            var getOptions = {
-                headers: {
-                    'Accept': 'application/json;odata=verbose'
+        return {
+            getItems: function () {
+                var getOptions = {
+                    headers: {
+                        'Accept': 'application/json;odata=verbose'
+                    }
                 }
-            }
-
-            var baseUri = 'https://techedairlift03.spoppe.com/expenses/_api/';          
                 
-               
-
-            return $http.get(baseUri + 'web/getfolderbyserverrelativeurl(\'Shared%20Documents\')/' +
+                var baseUri = 'https://techedairlift03.spoppe.com/expenses/_api/';
+                
+                
+                
+                return $http.get(baseUri + 'web/getfolderbyserverrelativeurl(\'Shared%20Documents\')/' +
                "files?$select=id,name,dateTimeCreated,webUrl,contentUrl&$orderby=name",
                getOptions).then(function (results) {
-                   var employees = results.data.d; //Get employees data
-
-                   return employees;
+                    var items = new Array();
+                    var files = results.data.d; //Get employees data
+                    for (var count = 0; count < files.length; count++) {
+                        items.push({
+                            name: files[count].Name,
+                            link: files[count].__metadata.id
+                        });
+                    }
+                   return items;
                }, function (error) {
                });
         }
