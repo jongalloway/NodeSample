c2137dfd69539817ad95b2285d111abaac5cf144
Jon Galloway <jongalloway@gmail.com>
2015-02-04 15:31:17 -0800

    Require New Relic

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-﻿var express = require('express');
+require('newrelic');
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
