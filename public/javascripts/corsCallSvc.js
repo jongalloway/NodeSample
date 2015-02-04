c262b005cbdb25be96e2e2de6a9dbbf96215c9b1
matvelloso <Ag-04650089>
2015-02-04 09:10:54 -0800

    Updates in the layout

--- public/javascripts/corsCallSvc.js
+++ public/javascripts/corsCallSvc.js
@@ -11,11 +11,10 @@ angular.module('corsApp')
                 
                 var baseUri = 'https://techedairlift03.spoppe.com/expenses/_api/';
                 
-                //https://techedairlift03.spoppe.com/expenses/_api/web/getfolderbyserverrelativeurl('Shared%20Documents')/files?$filter=substringof(%27Spread%27,Name)
                 
                 return $http.get(baseUri + 'web/getfolderbyserverrelativeurl(\'Shared%20Documents\')/' +
-               'files?$select=id,name,dateTimeCreated,webUrl,contentUrl&$filter=substringof(\'' + name + '\',Name)&$orderby=name',
-               getOptions).then(function (results) {
+                'files?$select=id,name,dateTimeCreated,webUrl,contentUrl&$filter=substringof(\'' + name + '\',Name)&$orderby=name',
+                getOptions).then(function (results) {
                     var items = new Array();
                     var files = results.data.d; //Get employees data
                     for (var count = 0; count < files.results.length; count++) {
