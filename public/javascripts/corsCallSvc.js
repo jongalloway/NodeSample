6f571909f5c6c95a22e8be8dd3efdb01580f77ec
matvelloso <Ag-04650089>
2015-02-03 17:53:23 -0800

    fixed hyperlink

--- public/javascripts/corsCallSvc.js
+++ public/javascripts/corsCallSvc.js
@@ -11,7 +11,7 @@ angular.module('corsApp')
                 
                 var baseUri = 'https://techedairlift03.spoppe.com/expenses/_api/';
                 
-                
+                //https://techedairlift03.spoppe.com/expenses/_api/web/getfolderbyserverrelativeurl('Shared%20Documents')/files?$filter=substringof(%27Spread%27,Name)
                 
                 return $http.get(baseUri + 'web/getfolderbyserverrelativeurl(\'Shared%20Documents\')/' +
                "files?$select=id,name,dateTimeCreated,webUrl,contentUrl&$orderby=name",
@@ -21,7 +21,7 @@ angular.module('corsApp')
                     for (var count = 0; count < files.results.length; count++) {
                         items.push({
                             name: files.results[count].Name,
-                            link: files.results[count].__metadata.uri
+                            link: files.results[count].__metadata.uri.replace('_api/Web/GetFileByServerRelativeUrl(\'/expenses/', '').replace('\')','')
                         });
                     }
                    return items;
