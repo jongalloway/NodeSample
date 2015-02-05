a947418eab5b3c3be4f2fb2cef7304713824fe32
Jon Galloway <jongalloway@gmail.com>
2015-02-05 13:45:56 -0800

    Simplified file query

--- public/javascripts/corsCallSvc.js
+++ public/javascripts/corsCallSvc.js
@@ -9,18 +9,16 @@ angular.module('corsApp')
                     }
                 }
                 
-                var baseUri = 'https://techedairlift03.spoppe.com/expenses/_api/';
+                var baseUri = 'https://techedairlift03.spoppe.com/expenses/_api/';                
                 
-                
-                return $http.get(baseUri + 'web/getfolderbyserverrelativeurl(\'Shared%20Documents\')/' +
-                'files?$select=id,name,lastModifiedBy,length,webUrl,contentUrl&$filter=substringof(\'' + name + '\',Name)&$orderby=name',
+                return $http.get(baseUri + 'files?$select=id,name,lastModifiedBy,Url',
                 getOptions).then(function (results) {
                     var items = new Array();
                     var files = results.data.d; //Get employees data
                     for (var count = 0; count < files.results.length; count++) {
                         items.push({
                             name: files.results[count].Name,
-                            link: files.results[count].__metadata.uri.replace('_api/Web/GetFileByServerRelativeUrl(\'/expenses/', '').replace('\')','') + '?web=1'
+                            link: files.results[count].Url + '?web=1'
                         });
                     }
                    return items;
