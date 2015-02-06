f4155393d0baf1c13051288c7ea8644545854104
Mat Velloso <matvelloso@users.noreply.github.com>
2015-02-05 18:01:28 -0800

    Update angularApp.js

--- public/scripts/angularApp.js
+++ public/scripts/angularApp.js
@@ -12,13 +12,12 @@ angular.module('docsApp', ['ngRoute','AdalAngular'])
 
     adalProvider.init(
         {
-            tenant: 'e0b38519-868b-4aa1-80d8-d1b3c7d75608',
-            clientId: 'd284e445-0c91-4538-9e16-3e0d33505329',
-            instance: 'https://login.windows-ppe.net/',
+            tenant: 'dpe1.onmicrosoft.com',
+            clientId: '00019913-06fa-48f6-91f6-5f959981687b',
             endpoints: [
             {
-                url: 'https://techedairlift03.spoppe.com/expenses/_api/',
-                resourceId: 'https://techedairlift03.spoppe.com'
+                url: 'https://dpe1-my.sharepoint.com/_api/',
+                resourceId: 'https://dpe1-my.sharepoint.com/
             }
             ]
         },
