38a2ee7333baa65db257632bb56762964ae1250e
Mat Velloso <matvelloso@users.noreply.github.com>
2015-02-05 18:27:05 -0800

    Update searchSvc.js

--- public/scripts/searchSvc.js
+++ public/scripts/searchSvc.js
@@ -6,18 +6,18 @@ angular.module('docsApp')
             getItems: function () {
                 var getOptions = {
                     headers: {
-                        'Accept': 'application/json'
+                        'Accept': 'application/json;odata=verbose;'
                     }
                 }
                 
-                var baseUri = 'https://dpe1-my.sharepoint.com/_api/v1.0/me/';                
+                var baseUri = 'https://dpe1-my.sharepoint.com/_api/me/';                
                 
                 return $http.get(baseUri + 'files?$select=id,name,lastModifiedBy,size,url',
                 getOptions).then(function (results) {
                     var items = new Array();
-                    var files = results.data.value;
+                    var files = results.data.d;
                     for (var count = 0; count < files.length; count++) {
-                        var item = files[count];
+                        var item = files.results[count];
                         items.push({
                             name: item.Name,
                             link: item.Url + '?web=1',
